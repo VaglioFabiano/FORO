ef8b3b5f89a5a0f517c02b525eb1d209a136cca2
Fabiano Vaglio <100426932+VaglioFabiano@users.noreply.github.com>
2025-06-21 10:45:57 +0200

    Trigger redeploy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
 import Header from './components/Header.tsx'
-import OrariSection from './components/OrariSection.tsx'
+/*import OrariSection from './components/OrariSection.tsx'
 import SocialSection from './components/SocialSection.tsx'
 import StatutoSection from './components/StatutoSection.tsx'
-import Footer from './components/Footer.tsx'
+import Footer from './components/Footer.tsx'*/
 
 function App(): JSX.Element {
   return (
     <div className="min-h-screen">
       <Header />
-      <OrariSection />
-      <SocialSection />
-      <StatutoSection />
-      <Footer />
+     
     </div>
   )
 }
