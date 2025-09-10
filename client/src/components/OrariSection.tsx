fbe9ea9937cf23c9df8e42e6925cb2cfb6f8f978
Fabiano Vaglio <100426932+VaglioFabiano@users.noreply.github.com>
2025-09-10 18:21:26 +0200

    2icona

--- client/src/components/OrariSection.tsx
+++ client/src/components/OrariSection.tsx
@@ -264,9 +264,9 @@ const OrariSection: React.FC = () => {
                   >
                     <div className="testo">
                       <span className="material-symbols-outlined map-icon">
-                        directions
+                        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=directions" />
+                        <strong>Ottieni Indicazioni</strong>
                       </span>
-                      <strong>Ottieni Indicazioni</strong>
                     </div>
                   </div>
                   
