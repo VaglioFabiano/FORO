c4d1a6b31c3a2ccf9e27aea43d6949182d717c8a
VaglioFabiano <s329986@studenti.polito.it>
2026-03-30 17:48:12 +0200

    css visualizzazione maggiore

--- client/src/dashboard/Contabilita.tsx
+++ client/src/dashboard/Contabilita.tsx
@@ -1,72 +1,54 @@
 import React from "react";
-import "../style/verbaliassemblea.css";
-// Assicurati di aver installato: npm install react-icons
+import "../style/contabilita.css";
 import { FcBarChart } from "react-icons/fc";
 import { SiGooglesheets } from "react-icons/si";
 import { IoOpenOutline } from "react-icons/io5";
 
 const Contabilita: React.FC = () => {
-  // ID estratto dal link fornito
   const sheetId = "1XbRRUG91LW3e_U___IPn090DNRAbGNTs";
 
-  // URL per l'incorporamento pulito (preview)
+  // URL per l'incorporamento pulito
   const embedUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/preview`;
 
   // URL per aprire il file originale su una nuova scheda
   const externalUrl = `https://docs.google.com/spreadsheets/d/${sheetId}/edit?usp=sharing`;
 
   return (
-    <div className="verbali_container">
+    <div className="contabilita_container">
       {/* HEADER */}
-      <div className="verbali_header">
+      <div className="contabilita_header">
         <FcBarChart size={40} />
-        <h2 className="verbali_title">Contabilità e Bilancio</h2>
+        <h2 className="contabilita_title">Contabilità e Bilancio</h2>
       </div>
 
       {/* CARD PRINCIPALE */}
-      <div
-        className="verbali_card"
-        style={{
-          minHeight: "75vh",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
+      <div className="contabilita_card">
         {/* BARRA SUPERIORE */}
-        <div className="verbali_nav">
-          <div className="verbali_nav-left">
-            <span className="verbali_path">
+        <div className="contabilita_nav">
+          <div className="contabilita_nav-left">
+            <span className="contabilita_path">
               Documento di riepilogo finanziario
             </span>
           </div>
 
-          <div style={{ display: "flex", gap: "10px" }}>
-            <a
-              href={externalUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="verbali_back-btn"
-              style={{ textDecoration: "none", color: "inherit" }}
-              title="Apri in Fogli Google"
-            >
-              <SiGooglesheets color="#0f9d58" size={18} />
-              <span>Apri Esternamente</span>
-              <IoOpenOutline size={18} style={{ marginLeft: "4px" }} />
-            </a>
-          </div>
+          <a
+            href={externalUrl}
+            target="_blank"
+            rel="noreferrer"
+            className="contabilita_external-btn"
+            title="Apri in Fogli Google"
+          >
+            <SiGooglesheets color="#0f9d58" size={18} />
+            <span>Apri Esternamente</span>
+            <IoOpenOutline size={18} style={{ marginLeft: "4px" }} />
+          </a>
         </div>
 
         {/* VISUALIZZATORE IFRAME */}
-        <div
-          className="verbali_preview-content"
-          style={{ flexGrow: 1, height: "100%", minHeight: "60vh" }}
-        >
+        <div className="contabilita_iframe-wrapper">
           <iframe
             src={embedUrl}
             title="Visualizzatore Contabilità"
-            width="100%"
-            height="100%"
-            style={{ border: "none", display: "block" }}
             allow="autoplay"
           ></iframe>
         </div>
